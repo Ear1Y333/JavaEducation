95cd2f06f49fba8fee3d456b7603d0c0c15f0751
ivan <ivan.kuzmiich@gmail.com>
2023-11-08 20:15:43 +0300

    commit

--- test1.java
+++ test1.java
@@ -1,12 +1,13 @@
 package skycapitals.text;
 
+import static java.lang.System.*;
 public class test1{
     public static void main(String[] args) {
-        int [] Numbers = {1, 2, 5};
-
-        System.out.println(GetSum(Numbers));
-        GetSum("Andrey", Numbers);
-        System.out.println(GetSum(1,2,3));
+        int [] numbers = {1, 2, 5};
+        out.println(GetSum(numbers));
+        GetSum("Andrey", numbers);
+        out.println(GetSum(1,2,3));
+        
     }
     public static int GetSum(int ...numbers){
         int sum = 0;
@@ -15,12 +16,12 @@ public class test1{
         }
         return sum;
     }
-    public static void GetSum(String Name, int ...numbers){
+    public static void GetSum(String name, int ...numbers){
         int sum = 0;
         for (int number : numbers){
             sum+=number;
         }
-        System.out.printf("Hi %s, the sum is %s\n", Name, sum);
+        out.printf("Hi %s, the sum is %s\n", name, sum);
     }
 
 }
