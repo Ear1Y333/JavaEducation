ac2ed3eac39d414232ce9f47e29d7e4fbf0d5aaa
ivan <ivan.kuzmiich@gmail.com>
2023-11-08 20:18:47 +0300

    commit

--- SpreadOperator.java
+++ SpreadOperator.java
@@ -1,13 +1,13 @@
 package skycapitals.text;
 
 import static java.lang.System.*;
-public class test1{
+public class SpreadOperator {
     public static void main(String[] args) {
         int [] numbers = {1, 2, 5};
         out.println(GetSum(numbers));
         GetSum("Andrey", numbers);
         out.println(GetSum(1,2,3));
-        
+
     }
     public static int GetSum(int ...numbers){
         int sum = 0;
