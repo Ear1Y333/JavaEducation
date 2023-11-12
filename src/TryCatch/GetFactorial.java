e59adc43d3a77ceca9a00cbf97397f403ae731ca
Ear1Y333 <troskin611@gmail.com>
2023-11-12 16:50:08 +0300

    Task3

--- src/TryCatch/GetFactorial.java
+++ src/TryCatch/GetFactorial.java
@@ -2,22 +2,22 @@ package TryCatch;
 
 public class GetFactorial {
     public static void main(String[] args) {
-        factorial(-15);
+        System.out.println(factorial(-15));
 
     }
-    public static void factorial(int number){
+    public static int factorial(int number){
+        int result=1;
         try {
             if (number<1) throw new Exception("Число не может быть меньше 1");
-            int result=1;
             for (int i=2; i!=number; i++){
                 result*=i;
-            System.out.println(result);;
             }
         }
         catch (Exception ex){
             System.out.println(ex.getMessage());
+            result = 0;
         }
-
+        return result;
     }
 }
 
